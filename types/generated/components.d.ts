e69a69040f435373cd511426959a5036104c5d67
graydon-whitespark <graydon@whitespark.ca>
2025-05-27 15:21:41 -0600

    create external link button

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,18 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface ElementsButtonLink extends Struct.ComponentSchema {
+  collectionName: 'components_elements_button_links';
+  info: {
+    displayName: 'Button Link';
+  };
+  attributes: {
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    link: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+    type: Schema.Attribute.Enumeration<['PRIMARY', 'SECONDARY']>;
+  };
+}
+
 export interface SeoMetaData extends Struct.ComponentSchema {
   collectionName: 'components_seo_meta_data';
   info: {
@@ -15,6 +28,7 @@ export interface SeoMetaData extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'elements.button-link': ElementsButtonLink;
       'seo.meta-data': SeoMetaData;
     }
   }
