b0b8c6a1a87e79576f84504f8624e6e1a5d10f39
graydon-whitespark <graydon@whitespark.ca>
2025-05-27 15:15:05 -0600

    create meta data component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,21 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SeoMetaData extends Struct.ComponentSchema {
+  collectionName: 'components_seo_meta_data';
+  info: {
+    displayName: 'Meta Data';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text;
+    metaImage: Schema.Attribute.Media<'images'>;
+    metaTitle: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'seo.meta-data': SeoMetaData;
+    }
+  }
+}
